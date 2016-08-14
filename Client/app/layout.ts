80b2e837cf44b87df6bd1878e3c32bdfde8a1d77
Tuấn Trần <ngoctuan291@gmail.com>
2016-08-14 10:57:40 +0700

    Start

---
+++ Client/app/layout.ts
@@ -0,0 +1,8 @@
+import {Component} from "angular2/core";
+import {RouteConfig, RouterOutlet, RouterLink} from "angular2/router";
+@Component({
+    selector: "default-layout",
+    templateUrl: "app/layout.html",
+})
+export class DefaultLayout {
+}
